d7218179decc290233c567bcec9adf40451418d8
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-02-03 18:39:06 +0200

    Change Deepwood Chest Boat item name translation
    you know i never noticed that somehow

--- src/client/java/com/aredl/datagen/BestestOfWoodsEnglishLangProvider.java
+++ src/client/java/com/aredl/datagen/BestestOfWoodsEnglishLangProvider.java
@@ -30,6 +30,6 @@ public class BestestOfWoodsEnglishLangProvider extends FabricLanguageProvider {
         translationBuilder.add("block.bow.deepwood_sign", "Deepwood Sign");
         translationBuilder.add("block.bow.deepwood_hanging_sign", "Deepwood Hanging Sign");
         translationBuilder.add("item.bow.deepwood_boat", "Deepwood Boat");
-        translationBuilder.add("item.bow.deepwood_chest_boat", "Deepwood Chest Boat");
+        translationBuilder.add("item.bow.deepwood_chest_boat", "Deepwood Boat with Chest");
     }
 }
